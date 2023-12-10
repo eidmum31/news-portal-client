d83457889c63f2625fdb6abf521495b5f046e79e
eidmum31 <mdzuneadabedineidmum@gmail.com>
2023-12-11 00:33:23 +0600

    login with github and google and social links

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import logo from "../../../assets/logo.png"
 import './Header.css'
 import moment from 'moment';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
+import user from '../../../assets/user.png';
+import { Link } from 'react-router-dom';
 const Header = () => {
 
 
@@ -17,11 +19,30 @@ const Header = () => {
             <div className="latest-news d-flex">
                 <Button variant="danger" className=''>Latest</Button>
 
-                <Marquee pauseOnHover={true} speed={100} className='text-dark fw-bold'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit Blanditiis iste accusamus expedita officiis excepturi Blanditiis iste accusamus expedita officiis excepturi.
+                <Marquee pauseOnHover={true} speed={100} className='text-danger'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit Blanditiis iste accusamus expedita officiis excepturi Blanditiis iste accusamus expedita officiis excepturi............
                 </Marquee>
 
             </div>
+            <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary my-3" data-bs-theme="light">
+                <Container>
+                    
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features"><Link className='link-underline-light text-dark' to="/">Home</Link></Nav.Link>
+                            <Nav.Link href="#pricing"><Link className='link-underline-light text-dark' to='/about'>About</Link></Nav.Link>
+                            <Nav.Link href="#pricing"><Link className='link-underline-light text-dark' to='/career'>Career</Link></Nav.Link>
+                            
+                     
+                        </Nav>
+                        <Nav>
+                            <img className='profile' src={user} alt="" />
+                            <Button variant="dark">Login</Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
--- src/pages/shared/RightNav/RightNav.jsx
+++ src/pages/shared/RightNav/RightNav.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebookF, FaTwitter ,FaInstagram  } from 'react-icons/fa';
 
 const RightNav = () => {
     return (
         <div>
-            This is right nav
+            <div>
+                <h4>Login With</h4>
+                <Button variant="outline-primary" className='mb-2'><FaGoogle />
+                    <span className='ms-2 my-auto'>Login with Google</span></Button>
+                <Button variant="outline-dark" className='mb-4' ><FaGithub /><span className='ms-2 my-auto'>Login with Github</span></Button>
+            </div>
+            <div>
+                <h4>Find Us On</h4>
+                <ListGroup as="ul">
+                    <ListGroup.Item as="li">
+                        <FaFacebookF /> <span className='ms-2 my-auto'>Facebook</span> 
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li">
+                        <FaTwitter /><span className='ms-2 my-auto'>Twitter</span>
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li">
+                       <FaInstagram /> <span className='ms-2 my-auto'>Instragam</span>
+                    </ListGroup.Item>
+
+                </ListGroup>
+            </div>
         </div>
     );
 };
